d614e57b613df010ef5f82e86f1beb9c2449a322
Your Name <gagankakar74@gmail.com>
2025-10-06 09:54:30 +0530

    minimal error fix

--- lib/interfaces/UserProfile.ts
+++ lib/interfaces/UserProfile.ts
@@ -33,7 +33,7 @@ export interface Video{
     preview_url:string;
     users:{
         id:number;
-        useername:string;
+        username:string;
         email:string;
         avatar_url:string;
     };
